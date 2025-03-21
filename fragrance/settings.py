1777ad72c6aae56b702781287161c9d1911203bd
minhaj-v-s <minhajshams3@gmail.com>
2025-03-21 12:05:48 +0530

    Deployment related code added to cart branch

--- fragrance/settings.py
+++ fragrance/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zl!0q^y64+-$7*g$^uyg&+h*l=oti1_24+4*)=5h@n#xnp#3xw'
+SECRET_KEY = 'jbs%lc#s7y9rj@l3kxng$jx-jklgo!z(bmn*5*uvd!h2+408mx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'fragrance.urls'
@@ -117,8 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR/'static']
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR/'static')]
 
 MEDIA_URL = "/images/"
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
